9e532af3baa0fa2557826420c2c0749245b9f228
Goran Peretin <goran.peretin@gmail.com>
2025-11-28 08:52:07 +0100

    set password for goran user

--- nixos/laptop/configuration.nix
+++ nixos/laptop/configuration.nix
@@ -141,11 +141,8 @@
 
   users.users = {
     goran = {
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
-      # initialPassword = "correcthorsebatterystaple";
       isNormalUser = true;
+      hashedPasswordFile = "/home/goran/Code/secrets/goran_password";
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
