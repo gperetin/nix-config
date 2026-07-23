9a4708b2a91868f183faeb6daf0c7be58e1699cc
Goran Peretin <goran.peretin@gmail.com>
2026-07-23 17:50:59 +0200

    open ghostty links in existing firefox

--- home-manager/desktop.nix
+++ home-manager/desktop.nix
@@ -16,6 +16,21 @@
   obsidianScale1 = gdkScale1 "obsidian-scale-1" "${pkgs-unstable.obsidian}/bin/obsidian";
   onePasswordScale1 = gdkScale1 "1password-scale-1" "${pkgs._1password-gui}/bin/1password";
   ghosttyScale1 = gdkScale1 "ghostty-scale-1" "${pkgs.ghostty}/bin/ghostty";
+  firefoxOpenUrl = pkgs.writeShellScriptBin "firefox-open-url" ''
+    # Ghostty runs at GDK scale 1, but Firefox belongs on the HiDPI display.
+    export GDK_SCALE=2
+    export GDK_DPI_SCALE=0.5
+    # Firefox currently runs the default profile in the XDG location. Specify
+    # it explicitly so this invocation is remote-controlled by that instance.
+    profile_root="''${XDG_CONFIG_HOME:-$HOME/.config}/.mozilla/firefox"
+    profile_path="$(awk -F= '$1 == "Path" { path = $2 } $1 == "Default" && $2 == "1" { print path; exit }' "$profile_root/profiles.ini")"
+
+    if [ -n "$profile_path" ]; then
+      exec ${pkgs.firefox}/bin/firefox --profile "$profile_root/$profile_path" --new-tab "$@"
+    fi
+
+    exec ${pkgs.firefox}/bin/firefox --new-tab "$@"
+  '';
 in {
   # You can import other home-manager modules here
   imports = [
@@ -48,6 +63,16 @@ in {
 
   # Override the system desktop entries so GUI launchers use a normal GTK scale.
   xdg.desktopEntries = {
+    # Ghostty opens Ctrl-clicked URLs through the default browser's desktop
+    # entry. Force Firefox's HiDPI environment and ask its running instance
+    # to add the URL as a tab rather than create a separately scaled window.
+    firefox = {
+      name = "Firefox";
+      exec = "${firefoxOpenUrl}/bin/firefox-open-url %U";
+      icon = "firefox";
+      categories = [ "Network" "WebBrowser" ];
+      mimeType = [ "text/html" "text/xml" "application/xhtml+xml" "x-scheme-handler/http" "x-scheme-handler/https" ];
+    };
     discord = {
       name = "Discord";
       exec = "${discordScale1}/bin/discord-scale-1 %U";
